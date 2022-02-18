65d494e9abede274b4efeb01d601e66ca9e45e73
parksil0 <sirisiri0809@gmail.com>
2022-02-19 00:40:56 +0900

    feat : 장바구니 아이콘 클릭 시 장바구니 페이지로 이동

--- client/src/pages/ProductList.tsx
+++ client/src/pages/ProductList.tsx
@@ -5,20 +5,31 @@ import { useActions } from "../hooks/useActions";
 import { useSelector } from "react-redux";
 import { RootState } from "../redux/reducers";
 import { Product } from "../types/product";
+import { useNavigate } from "react-router-dom";
 
 const ProductList = () => {
   const { getProducts } = useActions();
   const products = useSelector((state: RootState) => state.products);
 
+  const navigation = useNavigate();
+
   useEffect(() => {
     getProducts();
   }, [getProducts]);
+
+  const onClickCartIcon = (id: number) => {
+    navigation("/cart");
+  };
 
   return (
     <Layout>
       <section className="product-container">
         {products?.products?.map((product: Product) => (
-          <ProductListItem key={product.id} product={product} />
+          <ProductListItem
+            key={product.id}
+            product={product}
+            onClickCartIcon={() => onClickCartIcon(product.id)}
+          />
         ))}
       </section>
     </Layout>
