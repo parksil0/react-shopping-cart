d766107a8b98a7a8d34afb19ddb61f02a039ce8d
parksil0 <sirisiri0809@gmail.com>
2022-02-23 18:50:38 +0900

    feat : setting mock api with testing

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,12 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import App from "./App";
 
+// Start the mocking conditionally.
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("./mocks/browser");
+  worker.start();
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
